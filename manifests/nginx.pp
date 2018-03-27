ef80aaf2c67e9416cc9025bb980b3a6e2975b682
root <root@dshenkov6.mylabserver.com>
2018-03-27 08:33:38 +0000

    vhost hash

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,13 +1,12 @@
 class profile::nginx(
-# Array $vhosts,
+ Array $vhosts,
 ) {
   include ::nginx
   
- # $vhosts.each |$vhost| 
-#{
-    ::nginx::vhost { $facts['fqdn']:
-#      port           => $vhost['port'],
- #    server_aliases => $vhost['server_aliases'],
-  #  }
+ $vhosts.each |$vhost| {
+    ::nginx::vhost { $vhost['fqdn']:
+      port           => $vhost['port'],
+      server_aliases => $vhost['server_aliases'],
+  }
   }
 }
